f5e58bea60b4f417cec8d0494416ef38bbb17da3
Vasiliy Lyan <vasiliy.lyan@gmail.com>
2020-06-05 08:35:53 +0300

    removed most of VIM plugins

--- home/.vim/.install_vim_plugins.sh
+++ home/.vim/.install_vim_plugins.sh
@@ -3,13 +3,9 @@ pushd ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 pushd ~/.vim/bundle
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/tpope/vim-sensible.git
 git clone https://github.com/tpope/vim-unimpaired.git
-git clone https://github.com/will133/vim-dirdiff.git
 popd
 popd
